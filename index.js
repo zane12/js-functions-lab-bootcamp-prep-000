94015850d6ac24ef6f0803ade6b8996648e179bc
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-29 06:45:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(string) {
 function happyHolidayTo(holiday, name) {
   return 'Happy ' + holiday + ', ' + name +'!'
 }
+
+function holidayCountdown(holiday, days) {
+  return 'It\'s ' + days + ' until ' + holiday + '!'
+}
