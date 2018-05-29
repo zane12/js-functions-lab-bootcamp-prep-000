10f782efab6570d778939dd8a5a22a4927560e43
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-29 06:18:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(string) {
+  return 'Happy holidays, ${string}'
+}
+
+function happyHolidayTo(holiday, name) {
+  return 'Happy $holiday, ${name}!'
+}
